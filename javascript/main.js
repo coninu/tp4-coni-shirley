57f95f8da80d30440e10412862e4a5984785a8d2
Coni <coniarganaraz@hotmail.com>
2019-08-25 15:20:43 -0300

    probando en js

--- javascript/main.js
+++ javascript/main.js
@@ -1,6 +1,6 @@
 // API 
 
-const apiKey = '?api_key=280b6cef62614967a758507e49de17c9';
+const apiKey = '280b6cef62614967a758507e49de17c9';
 const baseUrl = 'https://api.themoviedb.org/3/movie/';
 const categoryPopular = 'popular';
 const categoryTopRated = 'top_rated';
@@ -20,9 +20,10 @@ const urlNowPlaying = `${baseUrl}${categoryNowPlaying}${apiKey}&page=${currentPa
 
 let homeBanner = document.getElementsByClassName('banner');
 let resultsContainer = document.getElementsByClassName('results');
-let textsInCategory = document.getElementsByClassName('category-header');
+let textSectionModel = document.getElementsByClassName('category-header');
 let categoryTitle = document.getElementsByClassName('category-name');
 let viewAllButton = document.getElementsByClassName('button-view-all');
+let contentContainer = document.getElementsByClassName('movie-container')
 
 // 2) Para las peliculas
 
@@ -31,9 +32,80 @@ let movieModel = document.getElementsByClassName('movie');
 let moviePosterDiv = document.getElementsByClassName('movie-poster');
 let moviePosterImg = document.getElementsByClassName('poster-image');
 let movieTitle = document.getElementsByClassName('movie-title');
+//let contentContainer = document.getElementsByClassName('movie-container')
 
 
 
-// Fetch para cada categoria simplificado en una funcion (no es para la home)
+
+// Creamos los elementos generales para el header de la home y peliculas
+
+const createTextSection = currentCategory => {
+    let textSectionModel = document.getElementsByClassName('category-header');
+    let categoryTitle = document.getElementsByClassName('category-name');
+    textSectionModel.innerHTML = '';
+    categoryTitle.innerHTML = currentCategory;
+    textSectionModel.appendChild(categoryTitle);
+    return textSectionModel
+}
+
+const createViewAllButton = container => {
+    let viewAllButton = document.getElementsByClassName('button-view-all')
+    container.appendChild(viewAllButton);
+    return viewAllButton
+}
+
+const createResultsNumberInfo = movies => {
+    let numberOfResults = document.createElement('h6');
+    numberOfResults.innerHTML = `${movies.total_results} results`;
+    return numberOfResults
+}
+
+
+// carga elementos de cada pelicula
+
+const loadMovieInfo = (movie, moviesContainer) => {
+    let movieModel = document.getElementsByClassName('movie');
+    let moviePosterDiv = document.getElementsByClassName('movie-poster');
+    if (movie.poster_path) {
+        moviePosterDiv.innerHTML = "<img class='poster-image' src= `https://image.tmdb.org/t/p/w500${movie.poster_path}";
+    } else {
+        moviePosterDiv.innerHTML = "<img class='poster-image' src='images/no-image.png'"
+    } 
+    
+    let movieTitle = document.getElementsByClassName('movie-title');
+    movieTitle.innerHTML = movie.title
+    // para el modal: newMovie.onclick = () => {moreInfo(movie.id)};
+    moviePosterDiv.appendChild(movieModel)
+    movieTitle.appendChild(movieModel)
+
+    moviesContainer.appendChild(movieModel);
+}
+
+
+// carga peliculas
+
+const homeMovies = (currentCategory, apiUrl) => {
+    fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => {
+        let movies = data.results.slice(0,5);
+        let resultsContainer = document.getElementsByClassName('results');
+        let textSectionModel = createTextSection(currentCategory);
+        createViewAllButton(textSection).onclick = () => {
+            changeMoviesInfo(currentCategory, apiUrl)
+        };
+        let moviesContainer = document.getElementsByClassName('movies');
+        resultsContainer.innerHTML = '';
+        moviesContainer.innerHTML = '';
+        resultsContainer.appendChild(textSectionModel);
+        for (let movie of movies){
+            loadBasicInfo(movie, moviesContainer)
+        }
+        resultsContainer.appendChild(moviesContainer);
+        contentContainer.appendChild(resultsContainer);
+    });
+}
+
+homeMovies('Popular Movies', `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}`);
 
 
